f1def697a70f023737bbbc2d7db524a0b4088706
HurrShahDev <hurrshah151214@gmail.com>
2026-06-03 18:33:47 +0500

    added Vite for assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
+
+    proxy: {
+      "/__l5e": {
+        target: "https://id-preview--38758152-3020-413a-a96c-9abee3a54eb9.lovable.app",
+        changeOrigin: true,
+      },
+    },
   },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
